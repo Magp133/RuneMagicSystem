c99c5224312b0fcb24a08fe27a9d464add7f43a0
Magp133 <ejrichardsward@gmail.com>
2024-01-29 19:47:05 +1300

    database

--- Database/database.gd
+++ Database/database.gd
@@ -21,12 +21,11 @@ func _ready():
 	populate_dictionary(rune_base_cache, "Runes")
 	populate_dictionary(saved_rune_cache, "SavedRunes")
 	
-	
-func query_database(table_name):
-	return db.query("select * from" + table_name + ";")
 
 func populate_dictionary(input_dictionary: Dictionary, table_name: String):
-	var data = query_database(table_name)
+	db.query("select * from " + table_name + ";")
+	var data = db.query_result
+	
 	
 	for i in range(0, data.size()):
 		pass
