35fb3842a618e8b5186593f166ef9463658d97ae
Magp133 <ejrichardsward@gmail.com>
2024-02-11 14:47:06 +1300

    all spells work

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 class_name Player
 
 @export var spell_scene: PackedScene
+@onready var spell_spawn = $SpellSpawn
+@onready var spell_mark = $SpellSpawn/SpellMark
+
+
+
 var spells: Dictionary = {}
 
 #need to have player stats to change attributes of spells and movement etc.
@@ -11,21 +16,40 @@ const SPEED = 300.01
 
 
 func _physics_process(_delta):
+	spell_spawn.look_at(get_global_mouse_position())
 	var keys = spells.keys()
-	
 	
 	for key in keys:
 		if Input.is_action_just_pressed(key):
 			var new_spell = spell_scene.instantiate()
 			new_spell.spell_data = spells[key]
+			
 			if spells[key]["Base"] == "Triangle":
+				"""
+				get direction for the movement.
+				look at mouse.
+				"""
+				#no clue why it needs the *3 but it doesnt work unless its out there.
+				new_spell.position = spell_mark.global_position * 3
 				new_spell.direction = global_position.direction_to(get_global_mouse_position())
 				new_spell.look_at(get_global_mouse_position())
+				
+				
+				add_child(new_spell, true)
+				
+				#new_spell.position = position
+			elif spells[key]["Base"] == "Square":
+				new_spell.position = spell_mark.global_position
+				new_spell.look_at(get_global_mouse_position())
+				get_parent().add_child(new_spell, true)
+			
+			
 			else:
 				new_spell.position = get_global_mouse_position()
+				get_parent().add_child(new_spell, true)
 			
 			
-			get_parent().add_child(new_spell)
+			
 	
 	velocity = Vector2.ZERO
 	# Get the input direction and handle the movement/deceleration.
--- Scenes/Spells/spell.gd
+++ Scenes/Spells/spell.gd
@@ -11,7 +11,7 @@ var spell_range: int
 var duration: float
 var complexity
 
-var speed: int = 10
+var speed: int = 500
 var size: int = 40
 
 var direction = Vector2.ZERO
@@ -21,13 +21,21 @@ var direction = Vector2.ZERO
 @onready var effect = $Effect
 @onready var life_time = $LifeTime
 
+#particles
 @export var explosion: ParticleProcessMaterial
 @export var burning: ParticleProcessMaterial
+@export var line: ParticleProcessMaterial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	effect.trail_enabled = false
+	
 	#area base effect
 	if spell_data["Base"] == "Circle":
+		"""
+		spell type is a stationary aoe that spawns on the cursor
+		"""
 		spell_type = "aoe"
 		var new_shape = CircleShape2D.new()
 		duration  = 2
@@ -35,13 +43,24 @@ func _ready():
 		new_shape.radius = size
 		hitbox_shape.shape = new_shape
 		effect.process_material = explosion
-		effect.set_lifetime(life_time.wait_time)
+		effect.set_lifetime(duration)
+		effect.amount = 200
 		effect.explosiveness = 1
+		
+		life_time.start()
 	
 	if spell_data["Base"] == "Triangle":
+		"""
+		spell type is a projectile that shoots towards the cursor.
+		"""
+		
 		spell_type = "projectile"
 		duration  = 4
 		life_time.wait_time = duration
+		effect.set_lifetime(0.5)
+		effect.amount = 150
+		effect.process_material = burning
+		
 		var new_shape = ConvexPolygonShape2D.new()
 		var triangle_points: PackedVector2Array = []
 		
@@ -49,24 +68,52 @@ func _ready():
 		var end_point = Vector2.from_angle(TAU / 2) * size / 2
 		var mid_point = Vector2.from_angle(-TAU / 4) * size 
 		
+		
 		#define hitbox of triangle
 		triangle_points = [start_point, end_point, mid_point,]
+		
+		
 		new_shape.set_point_cloud(triangle_points)
 		hitbox_shape.shape = new_shape
-		effect.set_lifetime(0.5)
-		effect.process_material = burning
+
+		
+		life_time.start()
+	
+	if spell_data["Base"] == "Square":
+		"""
+		spell type is a line that spawns from the player towards the mouse
+		"""
+		spell_type = "line"
+		duration = 3
+		life_time.wait_time = duration
+		effect.trail_enabled = true
+		effect.process_material = line
+		effect.set_lifetime(duration)
+		effect.amount = 50
+		
+		var new_shape = RectangleShape2D.new()
+		new_shape.size.y = 300
+		
+		
+		hitbox_shape.shape = new_shape
+		hitbox_shape.position.y = -150
+		
+		life_time.start()
+		
 		
 		
 		
 func _process(delta):
+	#pass
 	if spell_type == "projectile":
-		position += (position + direction) * speed * delta
+		position += direction * speed * delta
 
 func _on_hitbox_component_area_entered(area):
 	var attack = Attack.new()
 	attack.damage = power
 	attack.attack_position = global_position 
 	area.damage(attack)
+	
 
 
 func _on_life_time_timeout():
